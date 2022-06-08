2110863ab048260cb3ee0c4e85e8100f47a78f76
sh5800 <66026913+sh5800@users.noreply.github.com>
2022-06-08 23:02:31 +0530

    Transaction widget and initial styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,28 +8,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      title: 'Flutter App',
+      title: 'Personal Expenses App',
+      // theme: ThemeData(
+      //   primaryColor: Color(0xFF79DAE8)
+      // ),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'Item 1',
+      title: 'Ghost Backpack',
+      amount: 3400,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'Item 2',
+      title: 'Grocery',
+      amount: 3400,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         centerTitle: true,
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Card(
-            child: Text('CHART !'),
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: Card(
+              color: Colors.blue.shade100,
+              child: Text(
+                'CHART !',
+                textAlign: TextAlign.center,
+              ),
+              elevation: 5,
+            ),
           ),
-          Card(
-            child: Text('LIST of TRX'),
+          Container(
+            // width: double.infinity,
+            // height: 50,
+            child: Card(
+              color: Colors.red.shade100,
+              child: Text(
+                'LIST of TRX',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title.toString()),
+              );
+            }).toList(),
           ),
         ],
       ),
